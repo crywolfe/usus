1b1eb5a015537090377ab1092341dd746f4d4856
crywolfe <gwolfe2007@gmail.com>
2014-05-05 22:55:59 -0400

    added framework model for linkedin scraper

---
+++ app/models/linkedin_scraper.rb
@@ -0,0 +1,31 @@
+require 'time'
+require 'open-uri'
+require 'rack'
+
+class LinkedinScraper < ActiveRecord::Base
+
+attr_accessor :url, :searches
+
+  def initialize
+    @base_url = "http://search.gotobus.com/search/bus.do?"
+    @searches = []
+  
+  end
+
+  def build_query
+  end
+
+
+  def parse
+   
+
+  end
+
+  def fetch_html
+    # Load document using Nokogiri and open-uri
+    # save to instance variables the arrays of data
+    # to be iterated through for data capture
+    @doc = Nokogiri::HTML(open(@url))
+    
+  end
+end
