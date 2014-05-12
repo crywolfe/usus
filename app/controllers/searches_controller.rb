f6c1744f6a955e24781e4c2c7d544e04adea33e7
crywolfe <gwolfe2007@gmail.com>
2014-05-12 15:05:59 -0400

    continuing work on search results page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,16 @@
 class SearchesController < ApplicationController
+  def index
+
+  end
+
+
   def new
     @search = Search.new
   end
 
   def create
     @search = Search.create(search_params)
-    redirect_to root_path
+    redirect_to searches_path
   end
 
   private
